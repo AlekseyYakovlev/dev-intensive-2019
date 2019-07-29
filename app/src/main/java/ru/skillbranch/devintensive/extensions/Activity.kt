7edb8c8b92c8ddc1bf6cbc385fc2d790ed210e08
Aleksey <aleksey@yakovlev.spb.ru>
2019-07-29 20:54:56 +0300

    hometask_3 phase 5

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -9,13 +9,14 @@ fun Activity.hideKeyboard() {
     view?.let { v ->
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
         imm?.hideSoftInputFromWindow(v.windowToken, 0)
+
     }
 }
 
-fun Activity.isKeyboardOpen():Boolean {
- return true
+fun Activity.isKeyboardOpen(): Boolean {
+    val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+    //isInputViewShown()
+    return imm?.isAcceptingText ?: false
 }
 
-fun Activity.isKeyboardClosed():Boolean {
-    return true
-}
+fun Activity.isKeyboardClosed(): Boolean = !isKeyboardOpen()
